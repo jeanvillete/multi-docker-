dfb0f834f5203746bd97479f3c79ab8eaa9aa952
Jean Villete <jean.villete@gmail.com>
2019-03-06 23:21:58 -0300

    little change on the FE side.

--- client/src/App.js
+++ client/src/App.js
@@ -13,23 +13,15 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
-              Edit <code>src/App.js</code> and save to reload.
+              Fib Calculator
             </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
-            <div>
-              <Route exact path="/" component={ Fib } />
-              <Route path="/otherpage" component={ OtherPage } />
-            </div>
           </header>
+          <div>
+            <Route exact path="/" component={ Fib } />
+            <Route path="/otherpage" component={ OtherPage } />
+          </div>
         </div>
       </Router>
     );
